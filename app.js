fc159b51a0441f2af80330dc932880ff77b0e8fc
Anshuman Sharma <code4anshu@gmail.com>
2022-09-10 17:35:56 +0530

    update: app.js

--- app.js
+++ app.js
@@ -1,3 +1,24 @@
 var txtInput = document.querySelector("#ip-txt");
-var tranBtn = document.querySelector("#btn-translate");
+var transBtn = document.querySelector("#btn-translate");
 var txtOutput = document.querySelector("#op-txt");
+
+var translationAPI = "https://api.funtranslations.com/translate/shakespeare.json";
+
+function getURL(txt){
+    return translationAPI + "?" + txt;
+};
+
+function clickHandler(){
+    var txt = txtInput.value;
+
+    fetch(getURL(txt))
+    .then(response => response.json())
+    .then(json =>{
+        var translatedTxt = json.contents.translated;
+        txtOutput.textContent = translatedTxt;
+    })
+
+};
+
+
+transBtn.addEventListener("click", clickHandler);
