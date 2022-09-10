07e83497d22d9db7c35e2e330c133193f8fd30a6
Anshuman Sharma <code4anshu@gmail.com>
2022-09-10 23:48:19 +0530

    Update: app.js, Added: style.css

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 var txtInput = document.querySelector("#ip-txt");
 var transBtn = document.querySelector("#btn-translate");
 var txtOutput = document.querySelector("#op-txt");
+var counter = document.querySelector("#counter");
+
+
+var clck = 5;
 
 var translationAPI = "https://api.funtranslations.com/translate/shakespeare.json";
 
@@ -8,16 +12,40 @@ function getURL(txt){
     return translationAPI + "?text=" + txt;
 };
 
+function outOfMoves(){
+    alert("You're out of moves!! Only 5 are allowed :(");
+    console.log("error: out of moves");
+}
+
+function noText(){
+    alert("Kindly Enter some text");
+    console.log("error: out of moves");
+}
+
+function errorHandler(error){
+    alert("Error in fetching API or Moves Exhausted :(");
+}
+
 function clickHandler(){
     var txt = txtInput.value;
-
-    fetch(getURL(txt))
-    .then(response => response.json())
-    .then(json =>{
-        var translatedTxt = json.contents.translated;
-        txtOutput.textContent = translatedTxt;
-    })
-
+    if(txt.length == 0){
+        noText();
+    }
+    else if(clck == 0){
+        outOfMoves();
+    }
+    else{
+        clck--;
+        counter.textContent = clck;
+        fetch(getURL(txt))
+        .then(response => response.json())
+        .then(json =>{
+            var translatedTxt = json.contents.translated;
+            txtOutput.textContent = translatedTxt;
+            console.log(txtOutput.textContent);
+        })
+        .catch(errorHandler)
+    }
 };
 
 
